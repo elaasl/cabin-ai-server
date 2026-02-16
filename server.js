6a2ee0783dfa0b4d13f7f83825f65d6d63562a5b
elaasl <47537125+elaasl@users.noreply.github.com>
2026-02-16 19:41:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -42,4 +42,6 @@ app.post("/generate-images", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log("Server running on port " + PORT)
+);
